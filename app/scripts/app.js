565e035509a7d3cf51973ed2cb9a98d02c61caf9
taikai <297.1951@gmail.com>
2014-07-09 23:16:36 +0900

    change whole structure and add bootstrap

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ngStorage'
+    'ngStorage',
+    'ui.bootstrap'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/editor.js
+++ app/scripts/controllers/editor.js
@@ -12,70 +12,128 @@ angular.module('editorAngularApp')
 
     //use session strorage
     $scope.$storage=$sessionStorage;
+    if(! $scope.$storage.article){
+      $scope.$storage.article = {
+        title:"",
+        city:"",
+        contents:[]
+      };
+    }
 
-    $scope.$storage.draft=[];
+    if(! $scope.$storage.tools){
+      $scope.$storage.tools = {
+        link:{
+          type:"link"
+        },
+        heading:{
+          type:"heading"
+        },
+        paragraph:{
+          type:"paragraph"
+        }
+      };
+    };
+
+
+    // var itemFuncs = {
+    //   index:function(item){
+    //     return draft.indexOf(item);
+    //   },
+    //   html:function(item){
+    //     return '<'+item.tag+'>'+item.value+'</'+item.tag+'>';
+    //     console.log(draft)
+    //   },
+    //   append:function(item){
+    //     draft.splice(this.index(item)+1,0,newItem());
+    //   },
+    //   toggleTooltip:function(e,item){
+    //     e.stopPropagation();
+    //     item.showTooltip=!item.showTooltip;
+    //   },
+    //   delete:function(item){
+    //     draft.splice(draft.indexOf(item),1);
+    //   }
+    // };
+
+    $scope.format = {
+      link: function(link){
+        var html = '<a href = ' + link.url+ '>' + link.text +'</a>';
+        return html;
+      },
+      heading: function(heading){
+        var html = '<h3>' +heading.text + '</h3>'
+        return html;
+      },
+      title: function(title){
+        var html = '<h1>' +heading.text + '</h1>'
+        return html;
+      },
+      paragraph: function(paragraph){
+        var html = '<p>' +paragraph.text + '</p>'
+        return html;
+      }
+    }
+
+
+    var itemTemplate = {
+      value:"edit me!",
+      tag: "p",
+      showTooltip:false
+    }
+    var cities = [
+      "Ikebukuro",
+      "Shibuya",
+      "Shinjuku",
+      "Yebisu",
+      "Harajuku",
+      "test"
+    ]
+
+    var tags = [
+      {
+        label:"title",
+        value: "h1"
+      },
+      {
+        label:"heading",
+        value:"h2"
+      },
+      {
+        label:"sub heading",
+        value:"h3"
+      },
+      {
+        label:"paragraph",
+        value:"p"
+      }
+    ];
+
+    $scope.tags = tags;
+    $scope.cities = cities;
+
+    // if(! $scope.$storage.draft){
+    //   $scope.$storage.draft=[];
+    //   $scope.$storage.draft[0]=newItem();
+    // }
+
     //whole article draft
     var draft=$scope.$storage.draft;
 
-    function newItem(){
-      var itemTemplate =
-      {
-        value:"edit me!",
-        tag: "p",
-        show:false,
-        index:function(){
-          return draft.indexOf(this);
-        },
-        html:function()
-        {
-          return '<'+this.tag+'>'+this.value+'</'+this.tag+'>';
-        },
-        append:function()
-        {
-          draft.splice(this.index(),0,newItem())
-        }
-      }
-      return itemTemplate;
+
+    // $scope.itemFuncs=itemFuncs;
+
+
+
+    // merge obj2 to obj1 -- the same attr is overwittern by obj2
+    function merge(obj1, obj2) {
+      var obj3 = {};
+      for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+      for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+      return obj3;
     }
 
-    draft[0]=newItem();
-
-    // draft[0]={};
-    // draft[0].value="edit me"
-    // draft[0].tag="p"
-    // draft[0].parent=draft;
-    // draft[0].index=0;
-    // draft[0].html=function(){
-    //   var html='<'+this.tag+'>'+this.value+'</'+this.tag+'>'
-    //   return html
-    // }
-    // draft[0].append=function(){
-    //   this.draft.splice(this.index,0,newItem  )
-
-    // }
-
-
-    // $scope is a special object that makes
-    // its properties available to the view as
-    // variables. Here we set some default values:
-
-    $scope.showtooltip = true;
-
-    // Some helper functions that will be
-    // available in the angular declarations
-
-    $scope.hideTooltip = function(){
-
-      // When a model is changed, the view will be automatically
-      // updated by by AngularJS. In this case it will hide the tooltip.
-
-      draft.forEach(function(element,index,array){
-        element.show=false;
-      })
+    $scope.clone = function(obj){
+      return merge(obj,{});
     }
 
-    $scope.toggleTooltip = function(e){
-      e.stopPropagation();
-      $scope.showtooltip = !$scope.showtooltip;
-    }
   });
